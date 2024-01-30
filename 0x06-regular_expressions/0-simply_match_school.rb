129969dbad3ab8af2f84bf68d9d889ce636852aa
jessyovia <jessyovia100@gmail.com>
2024-01-30 14:14:48 -0800

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,6 +1,14 @@
 #!/usr/bin/env ruby
 
+# Accept the first command-line argument
+arg = ARGV[0]
+
+# Define the regular expression
+regex = /School/
+
 # Check if the argument matches the regular expression
-if ARGV[0] =~ /School/
-  puts ARGV[0]
+if arg =~ regex
+  puts arg
+else
+  puts "$"
 end
