2f392ebd576b96a73fec3e3cf0062c790176dc54
jessyovia <jessyovia100@gmail.com>
2024-03-22 18:01:54 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,6 @@
-# 1-install_a_package.pp
-# Puppet manifest to install the Flask package from pip3
+# Using Puppet install flask
 
-package { 'Flask':
-  ensure => '2.1.0',
+package {'Flask':
+  ensure   => '2.1.0',
   provider => 'pip3',
 }
-
