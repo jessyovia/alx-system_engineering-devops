ff0f0bd24b5a57c5ab69048a577cbdff93b164f4
jessyovia <jessyovia100@gmail.com>
2024-03-22 17:51:43 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,11 @@
+# 0-create_a_file.pp
 # Puppet manifest to create a file in /tmp
 
 file { '/tmp/school':
-  ensure   => present,
-  mode     => '0744',
-  owner    => 'www-data',
-  group    => 'www-data',
-  content  => 'I love Puppet',
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
 
