df9d8968b048f56cac868951746ef43078c397cc
jessyovia <jessyovia100@gmail.com>
2024-03-22 18:13:30 -0700

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,9 +1,6 @@
-# 2-execute_a_command.pp
-# Puppet manifest to kill a process named 'killmenow' using pkill
-
-exec { 'killmenow':
-  command     => 'pkill killmenow',
-  path        => '/usr/bin',
-  refreshonly => true,
+# Kills a process named killmenow
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
 
